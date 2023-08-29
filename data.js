5e83e4bc9d3a6bc8d0998a6d8ae7aed24e40fb9b
Shen Liu <tuh18904@temple.edu>
2023-08-28 20:48:13 -0400

    get data and sort them by date

---
+++ data.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const url = new URL("https://courses.ianapplebaum.com/api/syllabus/1");
+  
+    const headers = {
+      "Authorization": "Bearer eIGqgatYQzkNIksq6GHXpCPbG0Ra1M2JkXKSJStb", // Replace with your actual token
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    };
+  
+    fetch(url, {
+      method: "GET",
+      headers,
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Retrieve the array of events
+      const eventsArray = data.events || [];
+      
+      // Sort the events by date
+      eventsArray.sort((a, b) => new Date(a.event_date) - new Date(b.event_date));
+  
+      if (Array.isArray(eventsArray)) {
+        const container = document.getElementById('syllabusContainer');
+        
+        eventsArray.forEach(eventItem => {
+          const pItem = document.createElement('p');
+          pItem.innerHTML = `<strong class="d-block text-gray-dark">${eventItem.event_name || ''} <small>(${eventItem.event_date || ''})</small></strong> ${eventItem.event_description || ''}`;
+          container.appendChild(pItem);
+        });
+      } else {
+        console.error('eventsArray is not an array:', eventsArray);
+      }
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    });
+  });
+  
